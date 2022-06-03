79c1a0e924510621077970225bb9a9cad5dee0e8
Harutyun Piloyan <102599567+Piloyan@users.noreply.github.com>
2022-06-03 15:28:53 +0400

    Update Program.cs

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -7,8 +7,7 @@ namespace EnumerableExtensions
         static void Main(string[] args)
         {
             List<int> result = new() { 10, 19, 22, -5, -11, 28, 31, 3, 2, 1 };
-            var final = result.ToList();
-            _ = final.GetEnumerator();
+            _ = _First.GetEnumerator();
             _ = result._First();
             _ = result._Add(3);
             _ = result._Sub(2);
